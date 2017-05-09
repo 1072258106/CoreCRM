10a8dfd8ab35f33e94fef51a9fb4d7b59334acde
春寒 <381582176@qq.com>
2017-05-09 16:17:08 +0800

    async load

--- CoreCRM/ClientApp/src/views/Account/router.js
+++ CoreCRM/ClientApp/src/views/Account/router.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import {Router, Route, IndexRedirect} from 'dva/router';
-import Login from './Login';
+// import Login from './Login';
 
 export default (
-  <Route path="login" component={Login}/>
+  <Route path="login" getComponent={ (next, callback) => {
+    require.ensure([], () => {
+      // console.log(require('./Login'));
+      callback(null, require('./Login'))
+    }, 'Login');
+  } }/>
 );
 
